a9bdb25a6789d245014fbc068fb7322e413c0fbc
Serj-Lewa <serj.lewa@gmail.com>
2024-05-16 00:10:30 +0300

    s

--- test_print.py
+++ test_print.py
@@ -2,6 +2,7 @@ import time
 import os
 # time.sleep(10)
 print("4321")
-stream = os.popen('adb connect 127.0.0.1:5555')
+# stream = os.popen('adb connect 127.0.0.1:5555')
+stream = os.popen('ping 127.0.0.1 -c 10')
 output = stream.read()
 print(output)
