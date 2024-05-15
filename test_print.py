9906c81187208de5a923442f22eabee339eba1c8
Serj-Lewa <serj.lewa@gmail.com>
2024-05-16 00:20:04 +0300

    sad

--- test_print.py
+++ test_print.py
@@ -1,8 +1,18 @@
 import time
 import os
+import subprocess
+import shlex
+
 # time.sleep(10)
 print("4321")
 # stream = os.popen('adb connect 127.0.0.1:5555')
-stream = os.popen('ping -c 10 127.0.0.1')
-output = stream.read()
-print(output)
+# stream = os.popen('ping -c 10 127.0.0.1')
+# output = stream.read()
+# print(output)
+arg=shlex.split("adb exec-out screencap -p")
+process = subprocess.Popen(arg,
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+
+print(stdout)
