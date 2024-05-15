2245434c399c2af54c44739031e4dfbd8fff322f
Serj-Lewa <serj.lewa@gmail.com>
2024-05-16 00:11:12 +0300

    sadf

--- test_print.py
+++ test_print.py
@@ -3,6 +3,6 @@ import os
 # time.sleep(10)
 print("4321")
 # stream = os.popen('adb connect 127.0.0.1:5555')
-stream = os.popen('ping 127.0.0.1 -c 10')
+stream = os.popen('ping -c 10 127.0.0.1')
 output = stream.read()
 print(output)
