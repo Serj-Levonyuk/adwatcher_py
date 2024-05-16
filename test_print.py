a54561de14af1fa75fae75e2ccea4de806aa59e5
Serj-Lewa <serj.lewa@gmail.com>
2024-05-16 17:40:54 +0300

    w

--- test_print.py
+++ test_print.py
@@ -2,21 +2,22 @@ import time
 import os
 import subprocess
 import shlex
+import cv2
+from PIL import Image
 
 # time.sleep(10)
 print("4321")
+img = cv2.imread('/sdcard/1.png',cv2.IMREAD_GRAYSCALE)
 # stream = os.popen('adb connect 127.0.0.1:5555')
 # stream = os.popen('ping -c 10 127.0.0.1')
 # output = stream.read()
 # print(output)
 arg=shlex.split("adb -s 127.0.0.1 exec-out screencap -p")
 # arg=shlex.split("cat test_print.py")
-process = subprocess.Popen(arg,
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-stdout, stderr = process.communicate()
+# process = subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# stdout, stderr = process.communicate()
 b=b'\x2B'
-print(stdout)
-print(len(stdout))
-print(len(list(stdout)))
+# print(stdout)
+# print(len(stdout))
+# print(len(list(stdout)))
 # print(len(stdout))
