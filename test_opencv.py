5e26c5ae77d3bf53dce6165f4889236876f11ef3
Serj-Lewa <serj.lewa@gmail.com>
2024-07-25 17:56:33 +0300

    ggr

--- test_opencv.py
+++ test_opencv.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 
 img = cv2.imread('1.png')
-print(img.shape)
-img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+cv2.circle(img,500,500,200,(255,0,0),thickness=5)
 # template = cv2.imread('s.png')
 # w, h = template.shape[:-1]
 
@@ -13,4 +13,5 @@ img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # for pt in zip(*loc[::-1]):  # Switch columns and rows
     # cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-cv2.imwrite('2.png', img2)
+cv2.imwrite('2.png', img)
+print(img.shape)
