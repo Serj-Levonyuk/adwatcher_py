c8a393735800341b6b9a21ca5d6bad925a4b62d7
Serj-Lewa <serj.lewa@gmail.com>
2024-12-20 00:20:48 +0300

    ggh

--- test_doctr.py
+++ test_doctr.py
@@ -6,3 +6,7 @@ from doctr.models import ocr_predictor
 
 doc = DocumentFile.from_pdf("Cash-Payment-Receipt-Template.pdf")
 print(f"Number of pages: {len(doc)}")
+predictor = ocr_predictor(pretrained=True)
+print(predictor)
+result = predictor(doc)
+result.show()
